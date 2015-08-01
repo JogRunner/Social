2a9bab951aa7bf1e303adc45fb1c3dcde905e28c
JogRunner <jogrunner10@gmail.com>
2015-08-01 12:57:58 +0800

    submit setting page

---
+++ modules/users/social_user_setting.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * 注意：此文件由tpl_engine编译型模板引擎编译生成。
+ * 如果您的模板要进行修改，请修改 templates/default/modules/users/social_user_setting.html
+ * 如果您的模型要进行修改，请修改 models/modules/users/social_user_setting.php
+ *
+ * 修改完成之后需要您进入后台重新编译，才会重新生成。
+ * 如果您开启了debug模式运行，那么您可以省去上面这一步，但是debug模式每次都会判断程序是否更新，debug模式只适合开发调试。
+ * 如果您正式运行此程序时，请切换到service模式运行！
+ *
+ * 如有您有问题请到官方论坛（http://tech.jooyea.com/bbs/）提问，谢谢您的支持。
+ */
+?><?php
+
+	 $cur_menu = '3';
+	 
+	//引入语言包
+	$pu_langpackage=new publiclp;
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+	<p> 这是我的天下 </p>
+<?php require("uiparts/footor.php");?>
+</body>
+</html>
